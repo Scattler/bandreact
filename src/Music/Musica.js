11dd6127ed54a332c1ff3001e651e2b9b5de52bb
JJG <profecarmelitano@gmail.com>
2022-11-16 20:51:07 -0500

    segundo commit

--- src/Music/Musica.js
+++ src/Music/Musica.js
@@ -1,20 +1,64 @@
 import {servicioCanciones} from '../services/servicioCanciones.js'
 
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
+
 export function Music(){
 
+
 const [canciones,setCanciones]=useState(null)
+const[estamosCargando,setEstamosCargando]=useState(true)
 
-servicioCanciones()
-.then (function(respuesta){
+//Mi primer useeffect
+useEffect(function (){
 
+    servicioCanciones()
+    .then (function(respuesta){
+        setCanciones(respuesta)
+    
+    
+    })
+},[])
 
-
-}
+if(estamosCargando==true){
 
     return(
         <>
-            <h1>Music trabajando!!</h1>
+        <h1>estamos cargando...</h1>
+        
         </>
+
     )
+
+}else{
+    return(
+        <>
+        <h1>estan listas las canciones</h1>
+        {
+
+            canciones.tracks.map(function(){
+                return(
+                    <h5>{canciones.name}</h5>
+                )
+
+            })
+        }
+        </>
+
+    )
+}
+
+
+return(
+    <>
+    <h1>estan listas las canciones</h1>
+    {
+
+        canciones.tracks
+    }
+    
+    </>
+
+)
+
+    
 }
